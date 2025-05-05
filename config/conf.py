9747b1d3ab7db02aa7fa2d4f9cd1ff2f3a9b6688
yuramayer <votetopovorot2015@gmail.com>
2025-05-05 14:52:27 +0300

    perf: updated conf.py according to the pep8

--- config/conf.py
+++ config/conf.py
@@ -1,13 +1,19 @@
-from dotenv import load_dotenv, dotenv_values
+"""Loading the environment keys for the project"""
+
 import os
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
+
 def get_checked_env(env_name):
+    """Environment checker"""
     env = os.getenv(env_name)
     if not env:
         raise RuntimeError(f"The required variable isn't defined: {env_name}")
-    return str(env)     
+    return str(env)
+
 
 BOT_TOKEN = get_checked_env('BOT_TOKEN')
 BOOKS = get_checked_env('BOOKS')
