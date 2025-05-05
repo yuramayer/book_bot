41ee0dc39744cf0083615bbd39293d1c32bfd785
yuramayer <votetopovorot2015@gmail.com>
2025-05-05 14:53:06 +0300

    perf: updated bot.py according to the pep8

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
+"""Bot & Dispatcher settings with logger"""
+
 import logging
-from config import conf
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
+from config import conf
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - \
+                        %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 bot = Bot(
     token=conf.BOT_TOKEN, default=DefaultBotProperties(
         parse_mode=ParseMode.HTML))
 dp = Dispatcher(storage=MemoryStorage())
-
