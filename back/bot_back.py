521293557d4215730b8eea229112d245e026593a
yuramayer <votetopovorot2015@gmail.com>
2025-05-05 15:59:27 +0300

    perf: updated bot-back + cache according to the pep8

--- back/bot_back.py
+++ back/bot_back.py
@@ -1,9 +1,13 @@
+"""Methods working with text: pages & translations"""
+
 from nltk.tokenize import sent_tokenize
 import openai
 from config.conf import OPENAI_TOKEN
 
 
 def prettify_text(page, text):
+    """Prettify page before the sending"""
+
     txt = '\n\n'.join(sent_tokenize(text))
     txt += f'\n\n📖 <b>page {page}</b>\n\n'
     return txt
@@ -12,13 +16,15 @@ def prettify_text(page, text):
 CLIENT_GPT = openai.OpenAI(api_key=OPENAI_TOKEN)
 
 
-def translate_word(word):
+def translate_word(word: str) -> str:
+    """Translate the phrase with the ChatGPT"""
     question = CLIENT_GPT.chat.completions.create(
-        messages= [
+        messages=[
             {
                 'role': 'user',
                 'content': f'Переведи фразу на русский: "{word}".\
-                    При переводе учитывай, что она связана с "Властелином Колец"'
+                    При переводе учитывай, \
+                        что она связана с "Властелином Колец"'
             }
         ],
         model='gpt-4o'
--- back/cache.py
+++ back/cache.py
@@ -1,16 +1,21 @@
-from config.conf import books
+"""Cache for the bot"""
+
 import json 
+from config.conf import books
 
 BOOK_CACHE = {}
 BOOK_DICT = {}
 
 
-def load_book(book_title, book_dict):
+def load_book(book_title: str, book_dict: dict):
+    """Loading the json-book into the cache dict"""
+
     book_path = books.get(book_title)
     if not book_path:
-        print('Книга не найдена') 
+        print("The book wasn't found")
         return
-    with open(book_path, 'r') as book:
+
+    with open(book_path, 'r', encoding='utf-8') as book:
         book_json = book.read()
 
     book_dict[book_title] = json.loads(book_json)
