8d3c76c86846f21c3c92a769ab0dbf6e307cb159
yuramayer <votetopovorot2015@gmail.com>
2025-08-14 09:05:18 +0300

    fix: added try/except to the starting func

--- utils/starting.py
+++ utils/starting.py
@@ -1,16 +1,30 @@
 """Methods running when the bot is starting"""
 
+import logging
 from bot import bot
 from back.db_back import is_checked_db, create_books_database, \
     create_books_table, create_current_books_table, create_book_len_table
 from config.conf import admins_ids
 
 
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - \
+                        %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
 async def on_startup():
     """When bot is started, check db & send message to the admins"""
 
     for admin_id in admins_ids:
-        await bot.send_message(admin_id, 'Бот включён 😎')
+        try:
+            await bot.send_message(admin_id, 'Бот включён 😎')
+        except Exception as err:
+            logger.info(
+                "There was a problem with an id %s: %s",
+                admin_id,
+                err
+            )
 
     create_books_database()
     create_books_table()
@@ -19,8 +33,24 @@ async def on_startup():
 
     if not is_checked_db:
         for admin_id in admins_ids:
-            await bot.send_message(admin_id, 'База данных не работает 😢')
+            try:
+                await bot.send_message(admin_id,
+                                       'База данных не работает 😢')
+            except Exception as err:
+                logger.info(
+                    "There was a problem with an id %s: %s",
+                    admin_id,
+                    err
+                )
         return
 
     for admin_id in admins_ids:
-        await bot.send_message(admin_id, 'База данных успешно подключена 🤗')
+        try:
+            await bot.send_message(admin_id,
+                                   'База данных успешно подключена 🤗')
+        except Exception as err:
+            logger.info(
+                "There was a problem with an id %s: %s",
+                admin_id,
+                err
+            )
